4839700fb167787b66733b507cd411659c4e4ab9
Ferdia McKeogh <ferdia@mckeogh.tech>
2017-09-06 19:28:43 +0100

    Trying wiki method for HTTP server

--- main.go
+++ main.go
@@ -1,11 +1,32 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	//"fmt"
+	//	tf "github.com/tensorflow/tensorflow/tensorflow/go"
+	//	"github.com/tensorflow/tensorflow/tensorflow/go/op"
+	//	"io/ioutil"
+	"html/template"
 	"net/http"
 )
 
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	t, _ := template.ParseFiles("pages/" + tmpl + ".html")
+	t.Execute(w, p)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	renderTemplate(w, "index", p)
+}
+
 func main() {
-	fmt.Println("vim-go")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
